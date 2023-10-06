8f4a2fde800bd49afab95e42bbab2baaf2eae466
Kristie O <olds8434@vandals.uidaho.edu>
2023-10-06 13:30:35 -0700

    Added functionality to read input/output
    You can now read individual DI/DO registers

---
+++ examples/Digital_Input_Output.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('../') # This allows us to access files in src
+
+import src.FANUCethernetipDriver as EIP
+
+drive_path = '172.29.209.124' # Beaker
+
+# Read in a list of inputs
+inputList = EIP.readDigitalInputs(drive_path)
+print("Returned list")
+print(inputList)
+
+# Read in 1 input
+EIP.readDigitalInput(drive_path, 1) # DI[1]
+EIP.readDigitalInput(drive_path, 137) # DI[137]
+
+EIP.readDigitalOutput(drive_path, 5) # DO[5]
+
+
